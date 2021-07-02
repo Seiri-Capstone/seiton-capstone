d477f0e5b394960ba24601dc2de62e0c5c90d8fc
helsung <helenjeehosung@gmail.com>
2021-07-02 19:11:06 -0400

    single project api route

--- pages/api/kanban.js
+++
@@ -1,7 +0,0 @@
-import prisma from '../../prisma'
-
-export default async function handle(req, res) {
-  const { id } = req.body
-  const result = await prisma.project.get({ where: { id } })
-  res.json(result)
-}
---
+++ pages/api/project/[id].js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import prisma from '../../../prisma/prisma'
+
+export default async (req, res) => {
+  try {
+    const { id } = req.query
+    console.log(id)
+    const result = await prisma.project.findUnique({
+      where: { id: Number(id) },
+      include: {
+        columns: true
+      }
+    })
+    res.status(200).json(result)
+  } catch (error) {
+    console.log('error in the project id api call!', error)
+  }
+}
