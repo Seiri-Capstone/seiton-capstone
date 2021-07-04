c68babb1bc501e0030d5f432efbb8282c212eacd
Leo P <leon86ka@gmail.com>
2021-07-03 20:04:59 -0400

    beautiful dnd partially done

--- components/project/Column.js
+++ components/project/Column.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { tw } from 'twind'
 import Task from './Task'
-import { Droppable, Draggable } from 'react-beautiful-dnd'
+import { Droppable } from 'react-beautiful-dnd'
 
 export default function Column({ column }) {
   return (
-    <div>
-      <h1>{column.title}</h1>
-      {column.tasks.map(task => (
-        <Task task={task} key={task.id} />
-      ))}
+    <div className={tw`flex flex-col border border-gray-300`}>
+      <h5 className={tw`flex font-bold justify-center m-2 mb-5`}>
+        {column.title}
+      </h5>
+      <div
+        className={tw`mx-4 border border-gray-400 rounded-lg flex flex-col bg-white w-[200px] p-2 w-auto`}
+      >
+        <Droppable droppableId={String(column.id)}>
+          {provided => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {column.tasks.map((task, index) => {
+                return <Task task={task} key={task.id} index={index} />
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
     </div>
   )
 }
--- components/project/ProjectBoard.js
+++ components/project/ProjectBoard.js
@@ -12,14 +12,100 @@ export default function ProjectBoard() {
   useEffect(() => {
     dispatch(fetchProject(1))
   }, [project.id])
-  console.log(project)
+  console.log(project.columns)
+
+  //function needed to keep the state updated
+  const onDragEnd = result => {
+    const { destination, source, draggableId, type } = result
+    // //If there is no destination
+    if (!destination) {
+      return
+    }
+    // //If source and destination is the same
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    // If you're dragging columns
+    // const column = rightColumn(project.columns);
+    // const rightColumn = (project.columns) => {
+
+    // }
+    // if (type === 'column') {
+    //   dispatch(updateColumnOrder(result))
+    //   return
+    // }
+    // // Anything below this happens if you're dragging tasks
+    // const start = kanban.columns[source.droppableId]
+    // const finish = kanban.columns[destination.droppableId]
+    // // If dropped inside the same column
+    // if (start === finish) {
+    //   const tasks = [...start.taskIds]
+    //   const sourceIdx = source.index
+    //   const destIdx = destination.index
+    //   const colId = start.id
+    //   dispatch(
+    //     updateTaskOrderSameCol({
+    //       colId,
+    //       tasks,
+    //       sourceIdx,
+    //       destIdx,
+    //       draggableId
+    //     })
+    //   )
+    //   return
+    // }
+    // // If dropped in a different column
+    // const startTasks = [...start.taskIds]
+    // const finishTasks = [...finish.taskIds]
+    // const sourceIdx = source.index
+    // const destIdx = destination.index
+    // const startColId = start.id
+    // const finishColId = finish.id
+    // dispatch(
+    //   updateTaskOrderDiffCol({
+    //     startTasks,
+    //     finishTasks,
+    //     sourceIdx,
+    //     destIdx,
+    //     draggableId,
+    //     startColId,
+    //     finishColId
+    //   })
+    // )
+    // return
+    /////////////////////////////////////////////////////
+    // //example of result object
+    // const result = {
+    //   draggableId: 'task-1',  - user was dragging
+    //   type: 'TYPE',
+    //   reason: 'DROP',
+    //   whereDragStarted: {
+    //     droppableId: 'column-1',
+    //     index: 0
+    //   },
+    //   whereDragEnded: {
+    //     droppableId: 'column-1',
+    //     index: 1
+    //   }
+    // }
+    // //end of example
+    //////////////////////////////////////////////////
+  }
 
   return (
     <div>
-      {project.columns &&
-        project.columns.map(column => (
-          <Column key={column.id} column={column} />
-        ))}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className={tw`mx-auto flex justify-center `}>
+          {project.columns &&
+            project.columns.map(column => (
+              <Column key={column.id} column={column} />
+            ))}
+        </div>
+      </DragDropContext>
     </div>
   )
 }
