a86e2ea0eddcae252ea073a2a9098832df85bd38
helsung <helenjeehosung@gmail.com>
2021-07-04 22:46:01 -0400

    add task put route

--- pages/api/task/edit.js
+++ pages/api/task/edit.js
@@ -3,17 +3,18 @@ import prisma from '../../../prisma/prisma'
 export default async function putTask(req, res) {
   try {
     console.log("we're in the put task request!", req.body)
-    const { id, title, projectId, index } = req.body
-    // const result = await prisma.task.update({
-    //   where: { id: id },
-    //   data: {
-    //     title,
-    //     projectId,
-    //     index
-    //   }
-    // })
+    const { id, title, body, columnId, index } = req.body
+    const result = await prisma.task.update({
+      where: { id: id },
+      data: {
+        title,
+        body,
+        columnId,
+        index
+      }
+    })
 
-    res.status(200).json('meow')
+    res.status(200).json(result)
   } catch (error) {
     console.log('error in the column api call!', error)
   }
