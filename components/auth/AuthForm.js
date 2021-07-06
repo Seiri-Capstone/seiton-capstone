fcec1e0df98d7915c51f8250c456e1685adfa0d8
Swan <swanzhao@gmail.com>
2021-07-06 18:22:28 -0400

    added simple auth implementation

--- components/auth/AuthForm.js
+++ components/auth/AuthForm.js
@@ -1,52 +1,71 @@
 import React, { useState } from 'react'
 import { tw } from 'twind'
+import { signIn, signOut, useSession } from 'next-auth/client'
+import Link from 'next/link'
+import ProjectBoard from '../project/ProjectBoard'
 
 export default function AuthForm() {
+  const [session, loading] = useSession()
+  console.log('session', session)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  // console.log('process.env.GITHUB_ID', process.env.GITHUB_ID)
   const handleSubmit = evt => {}
+  if (!session) {
+    return (
+      <div className={tw`container mx-auto px-10 mt-72`}>
+        <h1>Organize your workflow.</h1>
+        <p className={tw`text-2xl`}>
+          Inspired by the 5S of Japanese workplace organization methodology,
+          <br />
+          Seiton will help you and your team to arrange your workflow
+          efficiently.
+        </p>
 
-  return (
-    <div className={tw`container mx-auto px-10 mt-72`}>
-      <h1>Organize your workflow.</h1>
-      <p className={tw`text-2xl`}>
-        Inspired by the 5S of Japanese workplace organization methodology,
-        <br />
-        Seiton will help you and your team to arrange your workflow efficiently.
-      </p>
-
-      <form className={tw`mt-8 max-w-md`}>
-        <div>
-          <label htmlFor="username" className={tw`text-gray-700`}>
-            Email
-          </label>
-          <input
-            name="email"
-            type="text"
-            className={tw`mt-1 block w-full rounded-md`}
-          />
-        </div>
-        <div>
-          <label htmlFor="password" className={tw`text-gray-700`}>
-            Password
-          </label>
-          <input
-            name="password"
-            type="password"
-            className={tw`mt-1 block w-full rounded-md`}
-          />
-        </div>
-        <div>
-          <button
-            type="submit"
-            className={tw`bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow`}
-          >
-            LOG IN
-          </button>
-        </div>
-        {/*error && error.response && <div> {error.response.data} </div>*/}
-      </form>
-    </div>
-  )
+        <form className={tw`mt-8 max-w-md`}>
+          <div>
+            <label htmlFor="username" className={tw`text-gray-700`}>
+              Email
+            </label>
+            <input
+              name="email"
+              type="text"
+              className={tw`mt-1 block w-full rounded-md`}
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className={tw`text-gray-700`}>
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              className={tw`mt-1 block w-full rounded-md`}
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className={tw`bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow`}
+              onClick={() => signIn()}
+            >
+              LOG IN
+            </button>
+          </div>
+          {/*error && error.response && <div> {error.response.data} </div>*/}
+        </form>
+      </div>
+    )
+  } else {
+    //redirect to dashboard
+    return (
+      <button
+        type="submit"
+        className={tw`bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow`}
+        onClick={() => signOut()}
+      >
+        SIGN OUT
+      </button>
+    )
+  }
 }
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,33 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+import Adapters from 'next-auth/adapters'
+import prisma from '../../../prisma/prisma'
+// import { NextApiHandler } from 'next'
+
+export default (req, res) => NextAuth(req, res, options)
+
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
+    // Providers.Email({
+    //   server: {
+    //     host: process.env.EMAIL_SERVER_HOST,
+    //     port: process.env.EMAIL_SERVER_PORT,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER,
+    //       pass: process.env.EMAIL_SERVER_PASSWORD
+    //     }
+    //   },
+    //   from: process.env.EMAIL_FROM
+    // })
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma })
+  // secret: process.env.SECRET
+}
