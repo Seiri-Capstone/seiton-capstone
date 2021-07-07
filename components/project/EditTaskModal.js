d502ccb31fe090322b9a79e5742734c077926824
helsung <helenjeehosung@gmail.com>
2021-07-07 16:59:11 -0400

    add markdown ability in task edit

--- components/project/EditTaskModal.js
+++ components/project/EditTaskModal.js
@@ -1,35 +1,61 @@
 import React, { useState } from 'react'
-import { tw, css } from 'twind/css'
+import { tw } from 'twind/css'
 import ReactDOM from 'react-dom'
+import marked from 'marked'
 
 export default function EditTaskModal({ task, show, onClose }) {
+  const [taskBody, setTaskBody] = useState(task.body)
+  const [isEditActive, setEditActive] = useState(false)
+
   if (!show) return null
   return ReactDOM.createPortal(
     <div
-      className={tw`justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none`}
+      className={tw`justify-center items-center flex flex-col overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none`}
     >
-      <div className={tw`relative w-auto my-6 mx-auto max-w-3xl`}>
+      <div className={tw`relative w-2/5 h-2/3 my-6 mx-auto `}>
         {/*content*/}
         <div
           className={tw`border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none`}
         >
           {/*header*/}
           <div
-            className={tw`flex items-start justify-between p-5 border-b border-solid rounded-t`}
+            className={tw`flex-col items-start p-5 border-b border-solid rounded-t`}
           >
             <h3 className={tw`text-3xl font-semibold`}>{task.title}</h3>
 
             {/*body*/}
             <div className={tw`relative p-6 flex-auto`}>
-              <textarea className={tw`my-4 text-lg leading-relaxed`}>
-                {task.body}
-              </textarea>
+              {isEditActive ? (
+                <textarea
+                  className={tw`my-4 text-lg leading-relaxed`}
+                  name="body"
+                  value={taskBody}
+                  onChange={e => setTaskBody(e.target.value)}
+                >
+                  {task.body}
+                </textarea>
+              ) : (
+                <div className={tw`my-6 mx-auto`}>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: marked(taskBody)
+                    }}
+                  ></div>
+                </div>
+              )}
             </div>
 
             {/*footer*/}
             <div
               className={tw`flex items-center justify-end p-6 border-t border-solid  rounded-b`}
             >
+              <button
+                className={tw`text-red-500  font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
+                type="button"
+                onClick={() => setEditActive(true)}
+              >
+                Edit
+              </button>
               <button
                 className={tw`text-red-500  font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
                 type="button"
@@ -40,6 +66,7 @@ export default function EditTaskModal({ task, show, onClose }) {
               <button
                 className={tw`text-blue-500  font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
                 type="button"
+                onClick={() => setEditActive(false)}
               >
                 Save Changes
               </button>
