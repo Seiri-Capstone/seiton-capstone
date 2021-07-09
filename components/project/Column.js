602425d131388251137370b9dbec92ea12e2c6f6
Leo P <leon86ka@gmail.com>
2021-07-09 13:31:58 -0400

    added options for delete button to either cancel or delete a task

--- components/project/Column.js
+++ components/project/Column.js
@@ -57,7 +57,7 @@ export default function Column(props) {
               <button
                 onClick={toggleNewTask}
                 value={props.value}
-                className="mr-4 px-2"
+                className="mr-4 px-2 focus:outline-none "
               >
                 <a>+</a>
               </button>
