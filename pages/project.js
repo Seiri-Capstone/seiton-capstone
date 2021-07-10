7f8188fbf4a9ebc4eb5c82e6166d25544e45e9de
helsung <helenjeehosung@gmail.com>
2021-07-10 00:07:43 -0400

    feat: navbar template

--- pages/project.js
+++ pages/project.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import dynamic from 'next/dynamic'
 const ProjectBoard = dynamic(import('../components/project/ProjectBoard'))
+import Navbar from '../components/project/Navbar'
 
 const Project = () => {
   return (
@@ -8,6 +9,7 @@ const Project = () => {
       <h1 className="font-ibm text-6xl font-bold text-red-800 dark:text-red-200 text-center mb-8">
         My Project
       </h1>
+      <Navbar />
       <ProjectBoard />
       <div id="modal"></div>
     </div>
