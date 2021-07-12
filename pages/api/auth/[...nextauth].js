f009d8b3a8d0c405300baba96e90e39d809bf77f
Sey <76843977+iseykim@users.noreply.github.com>
2021-07-12 16:16:48 -0400

    syntax

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,4 @@
-'import NextAuth from 'next-auth'
+import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 import Adapters from 'next-auth/adapters'
 import prisma from '../../../prisma/prisma'
