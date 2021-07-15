d71eba3a35caaf82c25d113744468004743cb534
Swan <swanzhao@gmail.com>
2021-07-15 15:16:42 -0400

    myorgs css style

--- components/org/MyOrgs.js
+++ components/org/MyOrgs.js
@@ -3,31 +3,83 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchOrgs } from '../../store/orgsSlice'
 import Link from 'next/link'
 import CreateOrgModal from './CreateOrgModal'
+import Logo from '../../components/Logo'
 
 export default function MyOrgs() {
   const orgs = useSelector(state => state.orgs)
   const dispatch = useDispatch()
   const [show, setShow] = useState(false)
 
+  console.log('orgs', orgs)
   useEffect(() => {
     dispatch(fetchOrgs())
   }, [dispatch])
 
+  const bgColors = ['bg-blue-500', 'bg-indigo-500', 'bg-purple-500']
+  const bgColors2 = ['bg-blue-100', 'bg-indigo-100', 'bg-purple-100']
+  const borderColors = [
+    'border-blue-500',
+    'border-indigo-500',
+    'border-purple-500'
+  ]
+
   return (
     <div>
-      <h1>List of my organizations</h1>
-      {orgs.map(org => (
-        <Link href={`/orgs/${org.id}`} key={org.id}>
-          <a>
-            <div>{org.name}</div>
-          </a>
-        </Link>
-      ))}
-      <div className="flex justify-end mr-12">
-        <button onClick={() => setShow(true)}>+Create Org</button>
+      <Logo />
+      <h1 id="tenor">My Organizations</h1>
+      <div>
+        <button onClick={() => setShow(true)}>+ Create Org</button>
       </div>
 
       <CreateOrgModal show={show} onClose={() => setShow(false)} />
+      <br />
+      {orgs?.map((org, i) => (
+        <>
+          <Link href={`/orgs/${org.id}`} key={org.id}>
+            <a>
+              <div className=" w-full lg:max-w-full lg:flex">
+                <div
+                  className={`h-48 lg:h-auto lg:w-12 flex-none rounded-t ${
+                    bgColors[i % 3]
+                  } lg:rounded-t-none lg:rounded-l text-center overflow-hidden`}
+                ></div>
+                <div
+                  className={`w-full border-r-4 border-b-4 border-l-4 ${
+                    borderColors[i % 3]
+                  } lg:border-l-0 lg:border-t-4 lg:${
+                    borderColors[i % 3]
+                  } bg-white
+                  } rounded-b lg:rounded-b-none lg:rounded-r p-8 flex flex-col justify-between leading-normal`}
+                >
+                  <div>
+                    <div
+                      id="tenor"
+                      className="text-gray-900 font-bold text-4xl mb-2 capitalize"
+                    >
+                      {org.name}
+                    </div>
+
+                    <b className="text-2xl leading-loose tracking-wide ml-4">
+                      Projects:
+                    </b>
+                    <br />
+                    <p className="text-lg tracking-wide ml-4">
+                      {org.projects.length
+                        ? org.projects.map(project => (
+                            <>
+                              • {project.name} <br />
+                            </>
+                          ))
+                        : 'Currently no projects are under this organization.'}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <br />
+        </>
+      ))}
     </div>
   )
 }
--- components/project/Navbar.js
+++ components/project/Navbar.js
@@ -10,62 +10,25 @@ export default function Navbar() {
   // https://ej2.syncfusion.com/react/demos/sidebar/docking-sidebar/index.html
 
   return (
-    <div
-      className={
-        isExpanded
-          ? 'flex flex-col sm:flex-row w-64 h-screen relative'
-          : 'flex flex-col sm:flex-row w-10 h-screen relative'
-      }
-    >
-      <div className="w-64 h-screen bg-gray-800 mt-8 sm:mt-0">
-        <ThemeSwitch />
-        <nav className="mt-10">
-          <button
-            className="text-gray-100"
-            onClick={() => setIsExpanded(!isExpanded)}
-          >
-            CLICK ME
-          </button>
-          <a
-            className="flex items-center py-2 px-8 bg-gray-700 text-gray-100 border-r-4 border-gray-100"
-            href="#"
-          >
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+    <>
+      <div
+        className={
+          isExpanded
+            ? 'flex flex-col sm:flex-row w-64 h-screen relative'
+            : 'flex flex-col sm:flex-row w-10 h-screen relative'
+        }
+      >
+        <div className="w-64 h-screen bg-gray-800 mt-8 sm:mt-0">
+          <ThemeSwitch />
+          <nav className="mt-10">
+            <button
+              className="text-gray-100"
+              onClick={() => setIsExpanded(!isExpanded)}
             >
-              <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path>
-            </svg>
-            <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
-              Dashboard
-            </span>
-          </a>
-
-          <Link href="/orgs">
-            <a className="flex items-center mt-5 py-2 px-8 text-gray-400 border-r-4 border-gray-800 hover:bg-gray-700 hover:text-gray-100 hover:border-gray-100">
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
-                Organizations
-              </span>
-            </a>
-          </Link>
-
-          <Link href="/projects">
+              CLICK ME
+            </button>
             <a
-              className="flex items-center mt-5 py-2 px-8 text-gray-400 border-r-4 border-gray-800 hover:bg-gray-700 hover:text-gray-100 hover:border-gray-100"
+              className="flex items-center py-2 px-8 bg-gray-700 text-gray-100 border-r-4 border-gray-100"
               href="#"
             >
               <svg
@@ -74,116 +37,155 @@ export default function Navbar() {
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path>
+                <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path>
               </svg>
-
               <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
-                Projects
+                Dashboard
               </span>
             </a>
-          </Link>
 
-          <a
-            className="flex items-center mt-5 py-2 px-8 text-gray-400 border-r-4 border-gray-800 hover:bg-gray-700 hover:text-gray-100 hover:border-gray-100"
-            href="#"
-          >
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M2 6a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 100 4v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2a2 2 0 100-4V6z"></path>
-            </svg>
-
-            <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
-              Tasks
-            </span>
-          </a>
-
-          <Link href="/invitations">
-            <a
-              className="flex items-center mt-5 py-2 px-8 text-gray-400 border-r-4 border-gray-800 hover:bg-gray-700 hover:text-gray-100 hover:border-gray-100"
-              href="#"
-            >
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2l-1 2H8l-1-2H5V5z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-
-              <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
-                Invitations
-              </span>
-            </a>
-          </Link>
-        </nav>
-
-        <div className="absolute bottom-0 my-10">
-          <a
-            className="flex items-center py-2 px-8 text-gray-600 hover:text-gray-500"
-            href="#"
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-              ></path>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              ></path>
-            </svg>
-            <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
-              Settings
-            </span>
-          </a>
-
-          <a
-            className="flex items-center py-2 px-8 text-gray-600 hover:text-gray-500"
-            href="#"
-          >
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <Link href="/user">
-              <a>My Profile</a>
+            <Link href="/orgs">
+              <a className="flex items-center mt-5 py-2 px-8 text-gray-400 border-r-4 border-gray-800 hover:bg-gray-700 hover:text-gray-100 hover:border-gray-100">
+                <svg
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
+                  Organizations
+                </span>
+              </a>
             </Link>
-          </a>
-          <button
-            onClick={() => signOut()}
-            className="flex items-center py-2 px-8 text-gray-600 hover:text-gray-500"
-          >
-            <span className="mx-4 font-medium">Sign Out</span>
-          </button>
+
+            <Link href="/projects">
+              <a
+                className="flex items-center mt-5 py-2 px-8 text-gray-400 border-r-4 border-gray-800 hover:bg-gray-700 hover:text-gray-100 hover:border-gray-100"
+                href="#"
+              >
+                <svg
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path>
+                </svg>
+
+                <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
+                  Projects
+                </span>
+              </a>
+            </Link>
+
+            <a
+              className="flex items-center mt-5 py-2 px-8 text-gray-400 border-r-4 border-gray-800 hover:bg-gray-700 hover:text-gray-100 hover:border-gray-100"
+              href="#"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2 6a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 100 4v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2a2 2 0 100-4V6z"></path>
+              </svg>
+
+              <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
+                Tasks
+              </span>
+            </a>
+
+            <Link href="/invitations">
+              <a
+                className="flex items-center mt-5 py-2 px-8 text-gray-400 border-r-4 border-gray-800 hover:bg-gray-700 hover:text-gray-100 hover:border-gray-100"
+                href="#"
+              >
+                <svg
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2l-1 2H8l-1-2H5V5z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+
+                <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
+                  Invitations
+                </span>
+              </a>
+            </Link>
+          </nav>
+
+          <div className="absolute bottom-0 my-10">
+            <a
+              className="flex items-center py-2 px-8 text-gray-600 hover:text-gray-500"
+              href="#"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                ></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                ></path>
+              </svg>
+              <span className={isExpanded ? 'mx-4 font-medium' : 'hidden'}>
+                Settings
+              </span>
+            </a>
+
+            <a
+              className="flex items-center py-2 px-8 text-gray-600 hover:text-gray-500"
+              href="#"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <Link href="/user">
+                <a>My Profile</a>
+              </Link>
+            </a>
+            <button
+              onClick={() => signOut()}
+              className="flex items-center py-2 px-8 text-gray-600 hover:text-gray-500"
+            >
+              <span className="mx-4 font-medium">Sign Out</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
