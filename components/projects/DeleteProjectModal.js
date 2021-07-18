5e47221b65289db31b4e08e8f1592ee9a1c8ac74
Swan <swanzhao@gmail.com>
2021-07-18 11:39:17 -0400

    css: consistent modal styling

--- components/projects/DeleteProjectModal.js
+++ components/projects/DeleteProjectModal.js
@@ -14,7 +14,7 @@ function DeleteProjectModal({ show, onClose, projectId }) {
 
   return show
     ? ReactDOM.createPortal(
-        <div className="justify-center items-center flex flex-col overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="justify-center items-center flex flex-col overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none -top-60">
           <Transition
             appear={true}
             show={show}
@@ -29,7 +29,7 @@ function DeleteProjectModal({ show, onClose, projectId }) {
           </Transition>
 
           <div className="relative w-100 h-40 my-6 mx-auto ">
-            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-900 outline-none focus:outline-none">
               <button
                 type="button"
                 className=" flex justify-end mr-3 focus:outline-none"
@@ -38,27 +38,27 @@ function DeleteProjectModal({ show, onClose, projectId }) {
                 onClick={onClose}
               >
                 <span
-                  className=" flex justify-end outline-none  border-solid"
+                  className=" flex justify-end outline-none m-2"
                   aria-hidden="true"
                 >
                   &times;
                 </span>
               </button>
-              <div className="flex-col items-start p-5 border-b border-solid rounded-t">
-                <h6 className="text-md font-semibold text-center">
-                  Are you sure you want to delete this project? You cannot undo
-                  this action.
+              <div className="flex-col items-start p-5 rounded-lg">
+                <h6 className="text-md tracking-wide mb-2  text-center">
+                  Are you sure you want to delete this project? <br />
+                  You cannot undo this action.
                 </h6>
-                <div className="flex items-center justify-center p-6 border-t border-solid  rounded-b">
+                <div className="flex items-center justify-center py-3">
                   <button
-                    className="mr-2 border px-2 rounded text-black-800 hover:text-red-500 border-red-500 focus:outline-none"
+                    className="bg-red-500 dark:bg-gray-600 text-white hover:bg-red-700 dark:text-gray-300 rounded-lg dark:hover:bg-red-500 mx-2 px-3 py-1 text-base shadow-sm"
                     type="button"
                     onClick={deleteOrg}
                   >
                     Delete
                   </button>
                   <button
-                    className="mr-2 border px-2 rounded text-black-800 hover:text-green-500 border-black-500 focus:outline-none"
+                    className="bg-skyblue dark:bg-gray-600 text-white hover:bg-navyblue dark:text-gray-300 rounded-lg dark:hover:bg-skyblue mx-2 px-3 py-1 text-base shadow-sm"
                     type="button"
                     onClick={onClose}
                   >
