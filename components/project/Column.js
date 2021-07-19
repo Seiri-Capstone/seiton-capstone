9c485ed3776171231c975701302e3ee52a1b00a2
Swan <swanzhao@gmail.com>
2021-07-19 10:31:07 -0400

    changed color placement in col

--- components/project/Column.js
+++ components/project/Column.js
@@ -66,37 +66,38 @@ export default function Column(props) {
     >
       {provided => (
         <div
-          className={`bg-${colors[colorI]}-300 w-64 h-full rounded-lg mr-3 p-4 flex flex-col`}
+          className={`bg-${colors[colorI]}-300 w-64 h-full rounded-lg mr-3 p-4 flex flex-col flex-grow`}
           ref={provided.innerRef}
           {...provided.draggableProps}
         >
-          <div className="flex flex-row justify-between items-center">
-            <div
-              className="flex flex-row items-center"
-              {...provided.dragHandleProps}
-            >
-              {isEditActive ? (
-                <input
-                  ref={input => input && input.focus()}
-                  className="px-2 text-base leading-relaxed w-32"
-                  name="body"
-                  value={columnName}
-                  onChange={e => setColumnName(e.target.value)}
-                  onKeyDown={handleKeyDown}
-                ></input>
-              ) : (
-                <div className="flex items-center dark:text-black">
-                  <span id="tenor" className="text-lg">
-                    {column.title}
-                  </span>
-                  <span id="tenor" className="ml-1">
-                    ({column.tasks.length})
-                  </span>
-                </div>
-              )}
+          <div className="flex flex-col h-full">
+            <div className="flex flex-row justify-between items-center">
+              <div
+                className="flex flex-row items-center mb-2"
+                {...provided.dragHandleProps}
+              >
+                {isEditActive ? (
+                  <input
+                    ref={input => input && input.focus()}
+                    className="px-2 text-base leading-relaxed w-32"
+                    name="body"
+                    value={columnName}
+                    onChange={e => setColumnName(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                  ></input>
+                ) : (
+                  <div className="flex items-center dark:text-black">
+                    <span id="tenor" className="text-lg">
+                      {column.title}
+                    </span>
+                    <span id="tenor" className="ml-1">
+                      ({column.tasks.length})
+                    </span>
+                  </div>
+                )}
 
-              {/* dots */}
-              {/* <svg
+                {/* dots */}
+                {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5 text-red-500"
                 viewBox="0 0 20 20"
@@ -105,82 +106,81 @@ export default function Column(props) {
               >
                 <circle cx="10" cy="10" r="10" />
               </svg> */}
-              {/* <Circle color={colors[colorI]} toggle={loopColor} /> */}
-              {/* ✏️ Pencil, cannot import with hero icon under draggable 🤷‍♂️ */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="ml-1 h-4 w-4 self-center"
-                viewBox="0 0 20 20"
-                fill="gray"
-                onClick={() => setEditActive(!isEditActive)}
-              >
-                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-              </svg>
+                {/* <Circle color={colors[colorI]} toggle={loopColor} /> */}
+                {/* ✏️ Pencil, cannot import with hero icon under draggable 🤷‍♂️ */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ml-1 h-4 w-4 self-center"
+                  viewBox="0 0 20 20"
+                  fill="gray"
+                  onClick={() => setEditActive(!isEditActive)}
+                >
+                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                </svg>
+              </div>
+
+              <div className="flex justify-end items-center">
+                <button
+                  onClick={toggleNewTask}
+                  value={props.value}
+                  className="mr-2 focus:outline-none "
+                >
+                  <a>+</a>
+                </button>
+
+                <button
+                  onClick={() => {
+                    setIsShowing(true)
+                  }}
+                  className="flex flex-col justify-center"
+                >
+                  <Image
+                    src={xIcon}
+                    alt="deleteIcon"
+                    className="self-center"
+                    width={20}
+                    height={20}
+                  />
+                </button>
+              </div>
             </div>
 
-            <div className="flex justify-end items-center">
-              <button
-                onClick={toggleNewTask}
-                value={props.value}
-                className="mr-2 focus:outline-none "
-              >
-                <a>+</a>
-              </button>
-              {/* SHOULD BE REFACTORED!!
-into dropdown see columndropdown.js */}
-              <button
-                onClick={() => {
-                  setIsShowing(true)
-                }}
-                className="flex flex-col justify-center"
-              >
-                <Image
-                  src={xIcon}
-                  alt="deleteIcon"
-                  className="self-center"
-                  width={20}
-                  height={20}
-                />
-              </button>
+            {toggleTask && (
+              <NewTask
+                addTaskSocket={addTask}
+                props={props}
+                toggleTask={toggleNewTask}
+              />
+            )}
+
+            <div className="h-full">
+              <Droppable droppableId={`column-${column.id}`} type="task">
+                {provided => (
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
+                    {column.tasks &&
+                      column.tasks.map((task, index) => (
+                        <>
+                          <Task
+                            setPin={setPin}
+                            task={task}
+                            key={task.id}
+                            index={index}
+                            taskEdit={taskEdit}
+                          />
+                        </>
+                      ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
             </div>
+            {/* color change logic */}
+
+            <hr
+              className={`mt-2 border-8 rounded-md border-${colors[colorI]}-500 justify-self-end`}
+              onClick={loopColor}
+            ></hr>
           </div>
-
-          {toggleTask && (
-            <NewTask
-              addTaskSocket={addTask}
-              props={props}
-              toggleTask={toggleNewTask}
-            />
-          )}
-
-          {/* color change logic */}
-          <hr
-            className={`mt-2 border-4 rounded-sm border-${colors[colorI]}-500 mb-3`}
-            onClick={loopColor}
-          ></hr>
-
-          <div className="">
-            <Droppable droppableId={`column-${column.id}`} type="task">
-              {provided => (
-                <div ref={provided.innerRef} {...provided.droppableProps}>
-                  {column.tasks &&
-                    column.tasks.map((task, index) => (
-                      <>
-                        <Task
-                          setPin={setPin}
-                          task={task}
-                          key={task.id}
-                          index={index}
-                          taskEdit={taskEdit}
-                        />
-                      </>
-                    ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </div>
-
           <DeleteColumnModal
             isShowing={isShowing}
             // toggle={toggle}
