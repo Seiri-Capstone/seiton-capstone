d3bf0e2cdcd43e3f7c5348758b081f973aeaf4b1
Helen Sung <helenjeehosung@gmail.com>
2021-07-19 15:02:59 -0400

    seed file

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,126 +2,91 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.org.create({
-    data: {
-      name: 'seiton-team',
-      projects: {
-        create: [
-          {
-            name: 'seiton-test-project',
-            columns: {
-              create: [
-                {
-                  title: 'to-do',
-                  index: 0,
-                  tasks: {
-                    create: [
-                      {
-                        title: 'task1',
-                        body: 'watch videos on next.js',
-                        index: 0
-                      },
-                      {
-                        title: 'task2',
-                        body:
-                          'practice using react hooks by refactoring class components',
-                        index: 1
-                      }
-                    ]
-                  }
-                },
-                {
-                  title: 'in-progress',
-                  index: 1,
-                  tasks: {
-                    create: [
-                      {
-                        title: 'task3',
-                        body: 'read documentation on redux toolkit',
-                        index: 0
-                      },
-                      {
-                        title: 'task4',
-                        body: 'write prisma schema',
-                        index: 1
-                      },
-                      {
-                        title: 'task5',
-                        body: '- [ ] seed the `prisma` **database**',
-                        index: 2
-                      }
-                    ]
-                  }
-                }
-              ]
-            }
-          },
-          {
-            name: 'seiton-test-project-2',
-            columns: {
-              create: [
-                {
-                  title: 'to-do',
-                  index: 0,
-                  tasks: {
-                    create: [
-                      {
-                        title: 'task1',
-                        body: 'stop procrastinating',
-                        index: 0
-                      },
-                      {
-                        title: 'task2',
-                        body: 'get to work',
-                        index: 1
-                      }
-                    ]
-                  }
-                },
-                {
-                  title: 'in-progress',
-                  index: 1,
-                  tasks: {
-                    create: [
-                      {
-                        title: 'task3',
-                        body: 'how to be centered',
-                        index: 0
-                      },
-                      {
-                        title: 'task4',
-                        body: 'how to be zen',
-                        index: 1
-                      }
-                    ]
-                  }
-                }
-              ]
-            }
-          }
-        ]
-      }
-    }
+  await prisma.user.createMany({
+    data: [
+      { name: 'Jane Doe', email: 'jane@gmail.com' },
+      { name: 'Ilana Wexler', email: 'ilana@gmail.com' },
+      { name: 'Frodo Baggins', email: 'frodo@gmail.com' }
+    ]
   })
 
   await prisma.org.create({
     data: {
-      name: 'bad trello',
+      name: 'Marketing Cat Company',
       projects: {
         create: [
           {
-            name: 'empty project',
+            name: 'Media Solutions Project',
             columns: {
               create: [
                 {
-                  title: 'to-do',
+                  title: 'To-Do',
                   index: 0,
                   tasks: {
                     create: [
                       {
-                        title: 'task1',
-                        body: 'task1',
+                        title: 'Negotiate Influencer campaign buys',
+                        body: 'Meet with Bustle and clients to find 2x influencers within budget for Summer campaign',
                         index: 0
+                      },
+                      {
+                        title: 'Create strategy for social following',
+                        body: '- [ ] Pull audience demographic report ',
+                        index: 1
+                      },
+                      {
+                        title: 'Reach out to Programmatic team for QoQ budgets',
+                        body: 'Need 2021 quarterly budgets for EOQ billing actualizations',
+                        index: 2
+                      }
+                    ]
+                  }
+                },
+                {
+                  title: 'In-Progress',
+                  index: 1,
+                  tasks: {
+                    create: [
+                      {
+                        title:
+                          'Create audience segmentation on portfolio brands',
+                        body: '- [ ] Pull audience demographic report from Tableau dashboard - [ ] Work with analytics team to get latest insights',
+
+                        index: 0
+                      },
+                      {
+                        title:
+                          'Gather client feedback on strategic brief for Seiton',
+                        body: 'Work with vendors for tweaks in media buys',
+                        index: 1
+                      },
+                      {
+                        title: 'Rotate in new creative assets',
+                        body: '- [ ] QA trafficking sheet from creative agency - [ ] Send confirmation screenshots to clients',
+                        index: 2
+                      }
+                    ]
+                  }
+                },
+                {
+                  title: 'Done',
+                  index: 2,
+                  tasks: {
+                    create: [
+                      {
+                        title: 'Update media flowchart',
+                        body: '- [ ] update flow w/ costs and imps - [ ] QA by supervisor - [ ] sent to clients',
+                        index: 0
+                      },
+                      {
+                        title: 'Meet with vendors from Hulu for CTV placement',
+                        body: 'Negotiate potential buys and sponsorships for repurposed portfolio assets',
+                        index: 1
+                      },
+                      {
+                        title: 'Idenitfy target search keyword categories',
+                        body: 'Generate list of top-performing search keywords for Google and Bing',
+                        index: 2
                       }
                     ]
                   }
